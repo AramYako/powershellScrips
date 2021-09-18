6c6ab088267bcad5307e4a3d7d311b6b6049c558
Aram Yako <aram.yako@volvocars.com>
2021-09-19 00:01:05 +0200

    added createWebAppScript

---
+++ Azure_Powershell_Scripts/CreateAzWebApp.ps1
@@ -0,0 +1,27 @@
+﻿$webappname = "mywebapp$(Get-Random)";
+$webAppLocation = " westus2";
+
+$createResourceGroup = $true
+$resourceGroupName = "DemoRgGroup2";
+
+$appServicePlaneName="DemoAzPlan";
+$tier = "S1"
+
+$addAppToResourceGroup ="DemoRgGroup2";
+
+
+#Create resource group
+ 
+if ($createResourceGroup -And $resourceGroupName) { #Check createResourceGroupAndName Is not empty
+    New-AzResourceGroup -Name  $resourceGroupName -Location $webAppLocation
+}
+
+
+#Create an App Service plan
+
+New-AzAppServicePlan -Name $appServicePlaneName -Location $webAppLocation -ResourceGroupName $addAppToResourceGroup -Tier $tier
+
+
+#Create a web app
+
+New-AzWebApp -Name $webappname -Location $webAppLocation -ResourceGroupName $addAppToResourceGroup -AppServicePlan $appServicePlaneName
